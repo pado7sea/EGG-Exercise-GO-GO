1ca3c036580a2ec77cfeb22db8650cc5ae7105eb
pado7sea <140683237+pado7sea@users.noreply.github.com>
2023-11-23 17:49:45 +0900

    Revert "맵"

--- front/dding_code/src/router/beforeEach.js
+++ front/dding_code/src/router/beforeEach.js
@@ -4,7 +4,7 @@ import { useUserStore } from '@/stores/user';
 import checkAuthentication from './checkAuthentication';
 
 export default function (to, from, next) {
-    // checkAuthentication(to, from, next);
+    checkAuthentication(to, from, next);
     const userStore = useUserStore(); // 항상 새로운 스토어 인스턴스 생성
     const isAuthenticated = userStore.loggedInUserId !== null && userStore.loggedInUserId !== undefined;
 
@@ -16,8 +16,8 @@ export default function (to, from, next) {
             // 로그인이 필요한데 사용자가 로그인되어 있지 않으면 알림 창 표시
             if (confirm('로그인이 필요한 페이지입니다. 로그인 하시겠습니까?')) {
                 // 사용자가 확인을 누르면 로그인 페이지로 이동
-                // console.log('Redirecting to login page.');
-                // console.log('User not authenticated. Redirecting to login page.');
+                console.log('Redirecting to login page.');
+                console.log('User not authenticated. Redirecting to login page.');
                 next('/');
             } else {
                 // 사용자가 취소를 누르면 현재 경로에서 이탈
@@ -25,12 +25,12 @@ export default function (to, from, next) {
             }
         } else {
             // 사용자가 로그인되어 있으면 허용
-            // console.log('User authenticated. Proceeding to the next route.');
+            console.log('User authenticated. Proceeding to the next route.');
             next();
         }
     } else {
         // 이 라우트에는 인증이 필요하지 않음
-        // console.log('Route does not require authentication. Proceeding to the next route.');
+        console.log('Route does not require authentication. Proceeding to the next route.');
         next();
     }
 }
