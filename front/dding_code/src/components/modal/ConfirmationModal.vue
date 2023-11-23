08e6c89c55b99b65a570815e102c92edc9ab7343
pado7sea <pado7sea@gmail.com>
2023-11-23 15:33:22 +0900

    라우터가드오류해결

---
+++ front/dding_code/src/components/modal/ConfirmationModal.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="confirmation-modal">
+      <div class="modal-content">
+        <p>{{ message }}</p>
+        <div class="buttons">
+          <button @click="confirm">확인</button>
+          <button @click="cancel">취소</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      message: String,
+      onConfirm: Function,
+      onCancel: Function
+    },
+    methods: {
+      confirm() {
+        this.onConfirm();
+      },
+      cancel() {
+        this.onCancel();
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* 스타일링을 원하는 대로 추가하세요. */
+  .confirmation-modal {
+    /* 모달 스타일링 */
+  }
+  
+  .modal-content {
+    /* 모달 내용 스타일링 */
+  }
+  
+  .buttons {
+    /* 버튼 스타일링 */
+  }
+  </style>
+  
--- front/dding_code/src/router/checkAuthentication.js
+++ front/dding_code/src/router/checkAuthentication.js
@@ -1,18 +1,20 @@
+// router/checkAuthentication.js
+
 import { useUserStore } from '@/stores/user';
 
 export default async function checkAuthentication(to, from, next) {
-    const userStore = useUserStore();
+  const userStore = useUserStore();
 
-    try {
-        // 비동기 작업 수행 (예: API 호출, 데이터 가져오기 등)
-        await userStore.checkAuthenticationAsync();
+  try {
+    // 사용자 정보를 가져와서 로그인 상태를 확인
+    await userStore.getUser();
 
-        // 비동기 작업이 완료되면 계속 진행
-        console.log('Authentication check successful. Proceeding to the next route.');
-        next();
-    } catch (error) {
-        // 에러가 발생하면 로그인 페이지로 리디렉션
-        console.error('Authentication check failed. Redirecting to login page.');
-        next('/login');
-    }
+    // getUser 메서드가 예외를 던지지 않으면(정상적으로 사용자 정보를 가져오면) 로그인 상태로 간주
+    console.log('Authentication check successful. Proceeding to the next route.');
+    next();
+  } catch (error) {
+    // getUser 메서드가 예외를 던지면 로그인하지 않은 상태로 간주
+    console.error('Authentication check failed. Redirecting to login page.');
+    next('/login');
+  }
 }
